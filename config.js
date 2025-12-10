891e5090acc0e232fcaeab8e6f2df7f96bfbc8ff
Rajesh Jadoun <deo01@ntwoods.com>
2025-12-10 15:14:09 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+GET https://ntwoods-com.github.io/test/config.js net::ERR_ABORTED 404 (Not Found)Understand this error
+app.js:41 Uncaught ReferenceError: google is not defined
+    at showLoginPage (app.js:41:5)
+    at initializeApp (app.js:27:9)
+    at HTMLDocument.<anonymous> (app.js:17:5)
+showLoginPage @ app.js:41
+initializeApp @ app.js:27
+(anonymous) @ app.js:17Understand this error
+favicon.ico:1  GET https://ntwoods-com.github.io/favicon.ico 404 (Not Found)
